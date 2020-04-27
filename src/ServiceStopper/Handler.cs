df07aaf1a9c66c41292025d885573a225c0a9909
Talen Fisher <talen.fisher@cythral.com>
2020-04-27 12:26:27 -0500

    set the assembly lambda serializer

--- src/ServiceStopper/Handler.cs
+++ src/ServiceStopper/Handler.cs
@@ -3,9 +3,9 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Amazon.Lambda.Core;
-using Amazon.ResourceGroups;
-using Amazon.ResourceGroups.Model;
 using Cythral.CloudFormation.Monitoring.ServiceStopper.ServiceUtils;
+
+[assembly:LambdaSerializer(typeof(System.Text.Json.JsonSerializer))]
 
 namespace Cythral.CloudFormation.Monitoring.ServiceStopper
 {
